e3c9453b27f20757064405b3fc1aba8e657d8bf1
ntampouratzis <32739524+ntampouratzis@users.noreply.github.com>
2022-03-06 18:31:45 +0200

    Update cossim_install.sh

--- cossim_install.sh
+++ cossim_install.sh
@@ -19,8 +19,10 @@ sudo apt update
 sudo apt install build-essential git m4 scons zlib1g zlib1g-dev libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev python3-dev python3-six python-is-python3 libboost-all-dev pkg-config
 
 ##certi prerequisites
-
+sudo apt install cmake bison flex libxml2-dev
 
 ##omnet 5.0 prerequisites
 sudo add-apt-repository ppa:rock-core/qt4 (https://ubuntuhandbook.org/index.php/2020/07/install-qt4-ubuntu-20-04/)
 sudo apt install openjdk-8-jdk openjdk-8-jre tcl-dev tk-dev qt4-qmake libqt4-dev libqt4-opengl-dev openmpi-bin libopenmpi-dev clang
+
+

7cabeecaf02035a9a50666b70acc66e8a77b121d
ntampouratzis <32739524+ntampouratzis@users.noreply.github.com>
2024-04-01 10:43:19 +0300

    Update cossim_install.sh

--- cossim_install.sh
+++ cossim_install.sh
@@ -1,17 +1,3 @@
-#Setup on Ubuntu 18.04
-sudo apt update
-
-##gem5 prerequisites
-sudo apt install build-essential git m4 scons zlib1g zlib1g-dev libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev python3-dev python3-six python libboost-all-dev pkg-config
-
-##certi prerequisites
-sudo apt install cmake bison flex libxml2-dev
-
-##omnet 5.0 prerequisites
-sudo apt install openjdk-8-jdk openjdk-8-jre tcl-dev tk-dev qt4-qmake libqt4-dev libqt4-opengl-dev openmpi-bin libopenmpi-dev clang
-
-
-
 #Setup on Ubuntu 20.04
 sudo apt update
 
@@ -23,6 +9,21 @@ sudo apt install cmake bison flex libxml2-dev
 
 ##omnet 5.0 prerequisites
 sudo add-apt-repository ppa:rock-core/qt4 (https://ubuntuhandbook.org/index.php/2020/07/install-qt4-ubuntu-20-04/)
+sudo apt install openjdk-8-jdk openjdk-8-jre tcl-dev tk-dev qt4-qmake libqt4-dev libqt4-opengl-dev openmpi-bin libopenmpi-dev clang
+
+
+#Setup on Ubuntu 22.04
+sudo apt update
+
+##gem5 prerequisites
+sudo apt install build-essential git m4 scons zlib1g zlib1g-dev libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev python3-dev python3-six python-is-python3 libboost-all-dev pkg-config
+
+##certi prerequisites
+sudo apt install cmake bison flex libxml2-dev
+sudo apt install cmake bison flex libxml2-dev libstdc++-12-dev
+
+##omnet 5.0 prerequisites
+sudo add-apt-repository ppa:ubuntuhandbook1/ppa (https://ubuntuhandbook.org/index.php/2020/07/install-qt4-ubuntu-20-04/)
 sudo apt install openjdk-8-jdk openjdk-8-jre tcl-dev tk-dev qt4-qmake libqt4-dev libqt4-opengl-dev openmpi-bin libopenmpi-dev clang
 
 
